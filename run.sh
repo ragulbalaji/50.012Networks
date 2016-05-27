923f493a76edf638a8735d621f434c4fce4b5220
Alexander Schaub <schaub.alexander@free.fr>
2016-05-26 23:03:07 -0700

    Updated run.sh script

--- run.sh
+++ run.sh
@@ -2,13 +2,14 @@
 declare -a arr=("1" "2" "4" "8" "16" "32" "64")
 
 # Launch the experiments
-sudo ./simulate_optack.py -d results/1 -n 1 -r 95 -t 30
+echo "Starting the experiments."
+sudo ./simulate_optack.py -d results/1 -n 1 -r 90 -t 30
 echo "Experiment with 1 server finished"
-sudo ./simulate_optack.py -d results/2 -n 2 -r 95 -t 30
+sudo ./simulate_optack.py -d results/2 -n 2 -r 90 -t 30
 echo "Experiment with 2 servers finished"
-sudo ./simulate_optack.py -d results/4 -n 4 -r 95 -t 30
+sudo ./simulate_optack.py -d results/4 -n 4 -r 90 -t 30
 echo "Experiment with 4 servers finished"
-sudo ./simulate_optack.py -d results/8 -n 8 -r 95 -t 30
+sudo ./simulate_optack.py -d results/8 -n 8 -r 90 -t 30
 echo "Experiment with 8 servers finished"
 sudo ./simulate_optack.py -d results/16 -n 16 -r 90 -t 30
 echo "Experiment with 16 servers finished"
@@ -23,7 +24,7 @@ for i in "${arr[@]}"
 do
     cd "$i"
     cd ../
-    python total-bytes-per-sec.py $i/*.csv > $i.csv
+    python ../plotting/total-bytes-per-sec.py $i/*.csv > $i.csv
 done
 
-gnuplot total-bytes-per-sec.plt
+gnuplot ../plotting/total-bytes-per-sec.plt
