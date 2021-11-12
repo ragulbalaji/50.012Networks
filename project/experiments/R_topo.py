4fd2ace1116ee926d7202252fc2c8bb82ecb4a14
Ragul Balaji <ragulbaby@gmail.com>
2021-11-12 23:37:39 +0800

    Update R_topo.py

--- project/experiments/R_topo.py
+++ project/experiments/R_topo.py
@@ -2,22 +2,23 @@ from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.cli import CLI
 
-class StarTopo(Topo):
+class TopoStar(Topo):
   def build(self, num_hosts=4):
-    centerSwitch = self.addSwitch( 's0' )
-
     hosts = []
+
+    centerSwitch = self.addSwitch('s0')
+    
     for i in range(num_hosts):
       hosts.append(self.addHost('h%d'%i))
     
-    links = []
     for host in hosts:
-      links.append(self.addLink(host, centerSwitch))
+      self.addLink(host, centerSwitch)
 
 
 if __name__ == '__main__':
-  topo = StarTopo(num_hosts=16)
+  topo = TopoStar(num_hosts=8)
   net = Mininet(topo)
   net.start()
+  net.pingAll()
   CLI(net)
   net.stop() 
