5f2be15cf8c059c9c622dba76b609a2cd95cd523
Ragul Balaji <ragulbaby@gmail.com>
2021-11-13 22:31:50 +0800

    testing multiple CC algos now!

--- project/experiments/SimpleStarTest.py
+++ project/experiments/SimpleStarTest.py
@@ -5,12 +5,12 @@ from mininet.net import Mininet
 from mininet.cli import CLI
 from mininet.node import OVSController
 
-from subprocess import Popen, PIPE
-from time import sleep, time
-from multiprocessing import Process
-from argparse import ArgumentParser
+# from subprocess import Popen, PIPE
+# from multiprocessing import Process
+# from argparse import ArgumentParser
 
 import time
+import os
 
 class TopoStar(Topo):
   def __init__(self, n=2, cpu=None, bw_atkr=10, bw_recv=10, bw_net=10, delay='100', maxq=None, diff=False):
@@ -23,15 +23,15 @@ class TopoStar(Topo):
     for i in range(n): self.addHost(f'h{i}', cpu=cpu)
     for i in range(n): self.addLink(f'h{i}', 's0', bw=bw_net, delay=delay)
 
-def ControlExperiment(hosts=8, test_time=10):
-  print("[Info] Starting Control Experiment")
+def ControlExperiment(expname=f'EXP_{time.time()}', hosts=4, test_time=10, transport_alg='-Z reno'):
   topo = TopoStar(n=hosts)
   net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, autoPinCpus=True, controller=OVSController)
   net.start()
   net.pingAll()
-  
+
+  print("[Info] Starting Control Experiment")
   # start tests
-  savedir = f'./results/{time.strftime("%Y%b%d_%H%M")}'
+  savedir = f'./results/{expname}/{transport_alg.replace(" ","_")}'
   atkr = net.getNodeByName('atkr')
   # setup recv
   recv = net.getNodeByName('recv')
@@ -40,15 +40,22 @@ def ControlExperiment(hosts=8, test_time=10):
   # setup others
   for i in range(hosts):
     hi = net.getNodeByName(f'h{i}')
-    hi.cmd(f'iperf -c {recv.IP()} -p 5001 -i 1 -w 16m -N -Z {"reno"} -t {test_time + 10} -y C > {savedir}/iperf_h{i}.csv &')
+    hi.cmd(f'iperf -c {recv.IP()} -p 5001 -i 1 -w 16m -N {transport_alg} -t {test_time + 10} -y C > {savedir}/iperf_h{i}.csv &')
   time.sleep(5) # delay start by 5 seconds
-  atkr.cmd(f'iperf -c {recv.IP()} -p 5001 -i 1 -w 16m -N -Z {"reno"} -t {test_time} -P 10 -y C > {savedir}/iperf_atkr.csv')
+  attacker_parallel_connections = 2
+  atkr.cmd(f'iperf -c {recv.IP()} -p 5001 -i 1 -w 16m -N {transport_alg} -t {test_time} -P {attacker_parallel_connections} -y C > {savedir}/iperf_atkr.csv')
   time.sleep(5) # delay  end  by 5 seconds
-  print("[Info] TEST ENDED, returning to CLI")
+  print("[Info] Test Ended")
   # tests end
 
-  CLI(net)
+  # CLI(net) # comment out when running main test
   net.stop()
 
 if __name__ == '__main__':
-  ControlExperiment()
+  ExperimentName = time.strftime("%Y%b%d_%H%M%S")
+  TransportAlgos = ['-Z reno', '-Z cubic', '-u']
+  for algo in TransportAlgos:
+    print(f'[Test] Running {ExperimentName} with {algo} CCalgo...')
+    ControlExperiment(expname=ExperimentName, transport_alg=algo)
+  os.system(f'zip ./results/{ExperimentName}.zip -r ./results/{ExperimentName}/')
+  os.system(f'rm -rf ./results/{ExperimentName}') # remove small files so git doesnt get angry
