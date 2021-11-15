36c31f88b4f3d5285dbaac2785433a39058971e8
James R T <jamestiotio@gmail.com>
2021-11-15 21:18:36 +0800

    Add temporary TCP Daytona experiment files

---
+++ project/experiments/tcp_daytona/plot.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import matplotlib
+
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+i = 0
+
+for cong_control in ["cubic", "reno", "vegas"]:
+    i += 1
+    DupFileName = "dupACK." + cong_control + ".npy"
+    SplitFileName = "splitACK." + cong_control + ".npy"
+    NormalFileName = "normal." + cong_control + ".npy"
+    OpFileName = "opACK." + cong_control + ".npy"
+
+    dupACKs = np.load(DupFileName)
+    splitACKs = np.load(SplitFileName)
+    normalACKs = np.load(NormalFileName)
+    opACKs = np.load(OpFileName)
+
+    plt.figure(i)
+    plt.subplot(311)
+    plt.title("Duplicate ACKs (%s)" % cong_control)
+    plt.scatter(*zip(dupACKs), label="Duplicate ACKs")
+    plt.scatter(*zip(normalACKs), label="Normal")
+    plt.xlabel("time (s)")
+    plt.ylabel("Seq Number")
+    plt.legend(loc="lower right")
+
+    plt.subplot(312)
+    plt.title("Split ACKs (%s)" % cong_control)
+    plt.scatter(*zip(splitACKs), label="Split ACKs")
+    plt.scatter(*zip(normalACKs), label="Normal")
+    plt.xlabel("time (s)")
+    plt.ylabel("Seq Number")
+    plt.legend(loc="lower right")
+
+    plt.subplot(313)
+    plt.title("Optimistic ACKs (%s)" % cong_control)
+    plt.scatter(*zip(opACKs), label="Optimistic ACKs")
+    plt.scatter(*zip(normalACKs), label="Normal")
+    plt.xlabel("time (s)")
+    plt.ylabel("Seq Number")
+    plt.legend(loc="lower right")
+
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+    plt.savefig("data.%s.png" % cong_control)
