a77bf2714c6562300ef6c9880c9e9c9899adc646
Mark Huang <hhzeos@gmail.com>
2021-11-18 23:51:11 +0800

    Update

---
+++ project/experiments/aggressive_sender/tree_tcp.py
@@ -0,0 +1,159 @@
+import argparse
+import csv
+import subprocess
+from datetime import datetime
+from pathlib import Path
+from time import mktime, sleep
+
+from mininet.link import TCLink
+from mininet.log import info, lg, setLogLevel
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.util import dumpNodeConnections, quietRun
+
+from tree_topo import TreeTopoTCP
+from utils import clean_tcpprobe_procs, start_tcpprobe
+
+output_dir = "out"
+if not Path(output_dir).is_dir():
+    Path(output_dir).mkdir(parents=True)
+
+
+def tcp_tests(algs=["reno"], delays=2, iperf_runtime=10):
+    """Run the TCP congestion control tests.
+
+    :param  algs                List of strings with the TCP congestion control algorithms to test.
+    :param  delays              List of integers with the one-directional propagation delays to test.
+    :param  iperf_runtime       Time to run the iperf clients in seconds.
+    """
+    print(
+        f"*** Tests settings:\n - Algorithms: {algs}\n - delays: {delays}\n - Iperf runtime: {iperf_runtime}"
+    )
+    for alg in algs:
+        print(f"*** Starting test for algorithm={alg}...")
+        for delay in delays:
+            print(f"*** Starting test for delay={delay}ms...")
+
+            # Start tcp probe process
+            print("*** Starting tcpprobe recording...")
+            tcpprobe_proc = start_tcpprobe(f"{output_dir}/tcpprobe_{alg}_{delay}ms.txt")
+
+            # Create the net topology
+            print(f"*** Creating topology for delay={delay}ms...")
+            topo = TreeTopoTCP(delay=delay)
+
+            # Start mininet
+            net = Mininet(topo)
+            net.start()
+
+            # Get the hosts
+            hosts = {}
+            host_addrs = {}
+            for i in range(1, 17):
+                hostname = f"h{i}"
+                hosts[hostname] = net.get(hostname)
+                host_addrs[hostname] = hosts[hostname].IP()
+
+            print("Host addrs: {0}".format(host_addrs))
+
+            # Run iperf
+            popens = dict()
+
+            print(f"*** Starting server h16...")
+            popens["h16"] = hosts["h16"].popen(
+                [
+                    "iperf",
+                    "-s",
+                    "-p",
+                    "5001",
+                    "-w",
+                    "16m",
+                    "-y",
+                    "C",
+                    ">",
+                    f"{output_dir}/iperf_{alg}_server_{delay}ms.txt",
+                ],
+                shell=True,
+            )
+
+            # Client options:
+            # -i: interval between reports set to 1sec
+            # -l: length read/write buffer set to default 8KB
+            # -w: TCP window size (socket buffer size) set to 16MB
+            # -M: TCP MSS (MTU-40B) set to 1460B for an MTU of 1500B
+            # -N: disable Nagle's Alg
+            # -Z: select TCP Congestion Control alg
+            # -t: transmission time
+            # -f: format set to kilobits
+            # -y: report style set to CSV
+
+            print("*** Starting iperf clients...")
+            for i in range(2, 10):
+                print(f"*** Starting normal client h{i}...")
+                hostname = f"h{i}"
+                _host = hosts[hostname]
+                _server_addr = host_addrs["h16"]
+                popens[hostname] = _host.popen(
+                    f"iperf -c {_server_addr} -p 5001 -i 1 -w 16K -M 1460 -N -Z {alg} -t {iperf_runtime} -y C > {output_dir}/iperf_{alg}_{hostname}_{delay}ms.txt",
+                    shell=True,
+                )
+
+            print(f"*** Starting attacking client h1...")
+            attacker_hostname = "h1"
+            attacker_host = hosts[attacker_hostname]
+
+            popens[attacker_hostname] = attacker_host.popen(
+                f"iperf -c {_server_addr} -p 5001 -i 1 -w 16K -M 1460 -N -Z {alg} -t {iperf_runtime} -y C > {output_dir}/iperf_{alg}_{attacker_hostname}_{delay}ms.txt",
+                shell=True,
+            )
+
+            print(f"*** Waiting {iperf_runtime}sec for iperf clients to finish...")
+            for i in range(1, 10):
+                hostname = f"h{i}"
+                popens[hostname].wait()
+
+            print("*** Terminate the iperf servers and tcpprobe processes...")
+            popens["h16"].terminate()
+            tcpprobe_proc.terminate()
+
+            popens["h16"].wait()
+            tcpprobe_proc.wait()
+
+            print("*** Cleaning tcp probe processes...")
+            clean_tcpprobe_procs()
+
+            print("*** Stopping test...")
+            net.stop()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="TCP Test")
+    parser.add_argument(
+        "-a",
+        "--algorithms",
+        nargs="+",
+        default=["reno", "cubic"],
+        help="List TCP Congestion Control algorithms to test.",
+    )
+    parser.add_argument(
+        "-d",
+        "--delays",
+        nargs="+",
+        type=int,
+        default=[21, 81, 162],
+        help="List of backbone router one-way propagation delays to test.",
+    )
+    parser.add_argument(
+        "-i",
+        "--iperf-runtime",
+        type=int,
+        default=30,
+        help="Time to run the iperf clients.",
+    )
+    args = parser.parse_args()
+    setLogLevel("info")
+    tcp_tests(args.algorithms, args.delays, args.iperf_runtime)
+
+
+if __name__ == "__main__":
+    main()
---
+++ project/experiments/aggressive_sender/tree_topo.py
@@ -0,0 +1,79 @@
+from mininet.link import TCLink
+from mininet.topo import Topo
+
+
+class TreeTopoTCP(Topo):
+    def __init__(
+        self,
+        delay: int = 2,  # delay in ms
+        bw_infra: int = 1000,  # bandwidth in Mbps
+        bw_user: int = 50,  # bandwidth in Mbps
+        bw_attack: int = 1000,  # bandwidth in Mbps
+    ):
+        super(TreeTopoTCP, self).__init__()
+
+        infra_link_params = dict(
+            bw=bw_infra,  # bandwidth in Mbps
+            delay=f"{delay}ms",
+            max_queue_size=(21 * delay * 20) / 100,
+            use_htb=True,
+        )
+
+        normal_user_link_params = dict(
+            bw=bw_user,  # bandwidth in Mbps
+            delay=f"{delay}ms",
+            max_queue_size=80 * delay,
+            use_htb=True,
+        )
+
+        attacker_params = dict(
+            bw=bw_attack,  # bandwidth in Mbps
+            delay=f"{delay}ms",
+            max_queue_size=80 * delay,
+            use_htb=True,
+        )
+
+        # Create routers s1 to s5
+        s1 = self.addSwitch("s1")
+        s2 = self.addSwitch("s2")
+        s3 = self.addSwitch("s3")
+        s4 = self.addSwitch("s4")
+        s5 = self.addSwitch("s5")
+
+        # connect Layer 0 switch to Layer 1 switches
+        self.addLink(s1, s2, cls=TCLink, **infra_link_params)
+        self.addLink(s1, s3, cls=TCLink, **infra_link_params)
+        self.addLink(s1, s4, cls=TCLink, **infra_link_params)
+        self.addLink(s1, s5, cls=TCLink, **infra_link_params)
+
+        depth = 2
+        fanout = 4
+
+        # connect Layer 1 switches to Layer 2 hosts
+        hosts = {}
+        for i in range(fanout ** depth):
+            hostname = f"h{i+1}"
+            hosts[hostname] = self.addHost(hostname)
+
+        # client - attacker
+        self.addLink(s2, hosts["h1"], cls=TCLink, **attacker_params)
+
+        # client - normal users
+        for i in range(2, 5):
+            hostname = f"h{i}"
+            self.addLink(s2, hosts[hostname], cls=TCLink, **normal_user_link_params)
+
+        for i in range(5, 9):
+            hostname = f"h{i}"
+            self.addLink(s3, hosts[hostname], cls=TCLink, **normal_user_link_params)
+
+        for i in range(9, 13):
+            hostname = f"h{i}"
+            self.addLink(s4, hosts[hostname], cls=TCLink, **normal_user_link_params)
+
+        for i in range(13, 16):
+            hostname = f"h{i}"
+            self.addLink(s5, hosts[hostname], cls=TCLink, **normal_user_link_params)
+
+        # server - receiver
+        self.addLink(s5, hosts["h16"], cls=TCLink, **infra_link_params)
