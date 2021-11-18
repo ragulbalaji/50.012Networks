3cddc12274167fb2753138586c61e6f373641628
James R T <jamestiotio@gmail.com>
2021-11-18 19:31:59 +0800

    Add minor changes

--- project/experiments/misbehaving_receiver/run-experiment.sh
+++ project/experiments/misbehaving_receiver/run-experiment.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run with sudo!"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run with sudo!"
   exit
 fi
 
 echo "Building LWIP stacks."
-(cd lwip-tap; ./configure; make)
-(cd lwip-tap-defended; ./configure; make)
+(
+  cd lwip-tap
+  ./configure
+  make
+)
+(
+  cd lwip-tap-defended
+  ./configure
+  make
+)
 
 echo "Destroying existing mininet topology..."
 mn -c
@@ -21,16 +29,16 @@ python runner.py --server=kernel --client=dup-ack
 python runner.py --server=kernel --client=ack-division
 
 # Run the vanilla LWIP server against various attackers.
-python runner.py --server=lwip --client=kernel
-python runner.py --server=lwip --client=opt-ack
-python runner.py --server=lwip --client=dup-ack
-python runner.py --server=lwip --client=ack-division
+# python runner.py --server=lwip --client=kernel
+# python runner.py --server=lwip --client=opt-ack
+# python runner.py --server=lwip --client=dup-ack
+# python runner.py --server=lwip --client=ack-division
 
 # Run the defended LWIP server against the attackers.
-python runner.py --server=lwip-defended --client=kernel
-python runner.py --server=lwip-defended --client=opt-ack
-python runner.py --server=lwip-defended --client=ack-division
-python runner.py --server=lwip-defended --client=dup-ack
+# python runner.py --server=lwip-defended --client=kernel
+# python runner.py --server=lwip-defended --client=opt-ack
+# python runner.py --server=lwip-defended --client=ack-division
+# python runner.py --server=lwip-defended --client=dup-ack
 
 echo "Creating graphs..."
 python create_graphs.py
